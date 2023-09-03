3f36563814b101f3ae8bd4c893194f9095e0bec4
Arelia Maia Ashary <maiaarelia@gmail.com>
2023-09-03 10:15:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/semester3_pertemuan3_tugaslogin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.semester3_pertemuan3_tugaslogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Semester3_Pertemuan3_TugasLogin"
+include(":app")
+ 
